dc4fd17bde9824691ee4061f9e37bd4e3d866126
sittora <sittora@gmail.com>
2021-08-09 22:52:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'SITORA EVERMAN';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser} !`;
+let shortGreeting = `Welcome, ${currentUser.charAt(0)} !`;
